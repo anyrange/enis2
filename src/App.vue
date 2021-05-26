72a2118ee7f8bc430a33bb384e52a76434147ac9
wsehl <widesehl@gmail.com>
2021-05-26 10:17:09 +0600

    Revert "+cookie law"
    This reverts commit 146cfed72d896e3a5c823100be046bb6776971de.

--- src/App.vue
+++ src/App.vue
@@ -6,19 +6,16 @@
       </q-page-container>
     </q-layout>
     <update-notification />
-    <cookie-law theme="blood-orange--rounded"></cookie-law>
   </div>
 </template>
 
 <script>
 import UpdateNotification from "@/components/UpdateNotification";
-import CookieLaw from "vue-cookie-law";
 
 export default {
   name: "App",
   components: {
     UpdateNotification,
-    CookieLaw,
   },
 };
 </script>
