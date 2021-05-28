043070ed9f786f015d994b72be22de12f66e367b
Alex Sehl <widesehl@gmail.com>
2021-05-28 20:49:50 +0600

    -sass

--- src/App.vue
+++ src/App.vue
@@ -16,4 +16,37 @@ export default {
 };
 </script>
 
-<style lang="scss" src="@/styles/styles.scss"></style>
+<style>
+@import url("https://fonts.googleapis.com/css2?family=Inter:wght@100;200;300;400;500;600;700;800;900&display=swap");
+#app {
+  font-family: "Inter", sans-serif;
+}
+.container {
+  width: 100%;
+  padding-right: 1rem;
+  padding-left: 1rem;
+}
+.q-tab-panel {
+  padding: 0px !important;
+}
+.mx-auto {
+  margin-left: auto;
+  margin-right: auto;
+}
+@media (min-width: 1024px) {
+  .lg\:w-1\/3 {
+    width: 33.333333%;
+  }
+}
+@media (min-width: 1280px) {
+  .xl\:w-1\/4 {
+    width: 25%;
+  }
+}
+.w-4 {
+  width: 1rem;
+}
+.h-4 {
+  height: 1rem;
+}
+</style>
--- src/quasar-user-options.js
+++ src/quasar-user-options.js
@@ -1,9 +1,7 @@
-import "./styles/quasar.sass";
-import "@quasar/extras/roboto-font/roboto-font.css";
+import "quasar/dist/quasar.css";
 import "@quasar/extras/material-icons/material-icons.css";
 import { Notify, Loading } from "quasar";
 
-// To be used on app.use(Quasar, { ... })
 export default {
   config: {},
   plugins: {
