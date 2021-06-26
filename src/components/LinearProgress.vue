114f2ac0b520e71e188ebad44e438be9fe4c6db1
Alex Sehl <widesehl@gmail.com>
2021-06-26 23:19:53 +0600

    feat: linear progress component

---
+++ src/components/LinearProgress.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="w-full dark:bg-gray-600-spotify rounded-full">
+    <div
+      class="h-1 rounded-l-full"
+      :style="{ width: value + '%' }"
+      :class="[strenthColor]"
+    >
+      &nbsp;
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "LinearProgress",
+  props: {
+    value: {
+      type: Number,
+      required: false,
+    },
+    color: {
+      type: String,
+      required: false,
+    },
+  },
+  computed: {
+    strenthColor() {
+      const roundedScore = Math.ceil(this.value);
+      if (roundedScore >= 85) return "bg-green";
+      if (roundedScore >= 65) return "bg-yellow";
+      return "bg-red";
+    },
+  },
+};
+</script>
+
+<style></style>
