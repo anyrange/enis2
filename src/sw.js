4a8ace71df3f2ea3718a6e74c132036f625c61bd
Alex Sehl <widesehl@gmail.com>
2021-06-29 17:18:02 +0600

    fix: service worker

--- src/sw.js
+++ src/sw.js
@@ -1,3 +1,7 @@
+importScripts(
+  "https://storage.googleapis.com/workbox-cdn/releases/5.0.0/workbox-sw.js"
+);
+
 import { precacheAndRoute } from "workbox-precaching";
 precacheAndRoute(self.__WB_MANIFEST);
 
