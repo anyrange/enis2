a9d23076b3ff4f2c1b15afce3e39fb63543328f5
Alex Sehl <widesehl@gmail.com>
2021-08-17 21:04:09 +0600

    fix: floating notifications

--- client/src/components/Notifications.vue
+++ client/src/components/Notifications.vue
@@ -58,6 +58,6 @@ export default {
 
 <style lang="postcss">
 .notifications-bottom-left {
-  @apply absolute bottom-4 left-4 z-40 overflow-x-hidden;
+  @apply fixed bottom-4 left-4 z-40 overflow-x-hidden;
 }
 </style>
