66a769b7d0bee94831b8a84dfd25afea754f2fe2
Alex Sehl <widesehl@gmail.com>
2021-10-07 12:31:47 +0600

    fix: loading dots animation

--- client/src/components/LoadingDots.vue
+++ client/src/components/LoadingDots.vue
@@ -13,8 +13,20 @@ export default {
 </script>
 
 <style lang="postcss" scoped>
+@keyframes bounce {
+  0%,
+  100% {
+    transform: translateY(-25%);
+    animation-timing-function: cubic-bezier(0.8, 0, 1, 1);
+  }
+  50% {
+    transform: translateY(0);
+    animation-timing-function: cubic-bezier(0, 0, 0.2, 1);
+  }
+}
 .circle {
-  @apply p-2 w-4 h-4 rounded-full animate-bounce;
+  @apply p-2 w-4 h-4 rounded-full;
+  animation: bounce 1s infinite;
 }
 .green-circle {
   animation-delay: 0.1s;
