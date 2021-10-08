72b07e299f0f6f09ada6858016ec86727205f00b
Alex Sehl <widesehl@gmail.com>
2021-10-08 14:37:25 +0600

    styles: update notification component

--- client/src/components/Notification.vue
+++ client/src/components/Notification.vue
@@ -5,37 +5,41 @@
       :style="progressStyle"
       class="notify-progress"
     />
-    <div class="notify-box">
-      <span class="notify-message">{{ notification.message }}</span>
-      <div v-if="notification.actions">
-        <button
-          v-for="(action, index) in notification.actions"
-          :key="`${action}-${index}`"
-          type="button"
-          class="notify-action-btn"
-          @click="handleAction(action.handler)"
-        >
-          {{ action.title }}
-        </button>
+    <div class="flex flex-row items-center space-x-2">
+      <div class="truncate">
+        {{ notification.message }}
       </div>
-      <span
-        v-if="notification.closable"
-        class="notify-close"
-        @click="closeNotification(notification)"
-      >
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          height="24px"
-          viewBox="0 0 24 24"
-          width="24px"
-          fill="currentColor"
+      <div class="flex items-center space-x-2">
+        <template v-if="notification.actions">
+          <button
+            v-for="(action, index) in notification.actions"
+            :key="`${action}-${index}`"
+            type="button"
+            class="notify-action-btn"
+            @click="handleAction(action.handler)"
+          >
+            {{ action.title }}
+          </button>
+        </template>
+        <span
+          v-if="notification.closable"
+          class="notify-close"
+          @click="closeNotification(notification)"
         >
-          <path d="M0 0h24v24H0V0z" fill="none" />
-          <path
-            d="M18.3 5.71c-.39-.39-1.02-.39-1.41 0L12 10.59 7.11 5.7c-.39-.39-1.02-.39-1.41 0-.39.39-.39 1.02 0 1.41L10.59 12 5.7 16.89c-.39.39-.39 1.02 0 1.41.39.39 1.02.39 1.41 0L12 13.41l4.89 4.89c.39.39 1.02.39 1.41 0 .39-.39.39-1.02 0-1.41L13.41 12l4.89-4.89c.38-.38.38-1.02 0-1.4z"
-          />
-        </svg>
-      </span>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            height="24px"
+            viewBox="0 0 24 24"
+            width="24px"
+            fill="currentColor"
+          >
+            <path d="M0 0h24v24H0V0z" fill="none" />
+            <path
+              d="M18.3 5.71c-.39-.39-1.02-.39-1.41 0L12 10.59 7.11 5.7c-.39-.39-1.02-.39-1.41 0-.39.39-.39 1.02 0 1.41L10.59 12 5.7 16.89c-.39.39-.39 1.02 0 1.41.39.39 1.02.39 1.41 0L12 13.41l4.89 4.89c.39.39 1.02.39 1.41 0 .39-.39.39-1.02 0-1.41L13.41 12l4.89-4.89c.38-.38.38-1.02 0-1.4z"
+            />
+          </svg>
+        </span>
+      </div>
     </div>
   </div>
 </template>
@@ -106,7 +110,7 @@ export default {
 
 <style lang="postcss">
 .notify {
-  @apply relative mt-4 p-2 table rounded text-sm shadow-md drop-shadow-md;
+  @apply relative sm:w-auto w-full mt-4 p-2 table rounded text-sm shadow-md drop-shadow-md;
 }
 .notify-progress {
   @apply absolute bottom-0 left-0 h-1 rounded bg-white bg-opacity-50 z-50;
@@ -123,16 +127,10 @@ export default {
 .notify-danger {
   @apply bg-q-negative text-white;
 }
-.notify-box {
-  @apply flex flex-row items-center justify-between;
-}
-.notify-message {
-  @apply pl-2 pr-3;
-}
 .notify-close {
   @apply rounded-full text-gray-200 justify-center inline-flex duration-150 cursor-pointer p-1 hover:bg-gray-200 hover:bg-opacity-20;
 }
 .notify-action-btn {
-  @apply p-2 rounded-sm uppercase duration-100 hover:bg-white hover:bg-opacity-20;
+  @apply p-2 rounded-sm uppercase duration-100 hover:bg-white hover:bg-opacity-20 appearence-none focus:outline-none select-none;
 }
 </style>
