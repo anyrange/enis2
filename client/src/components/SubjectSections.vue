d168ce9c6aea4bff44e46cf8562f229673c90c1c
Alex Sehl <widesehl@gmail.com>
2021-11-28 00:59:37 +0600

    feat: percents on subject sections

--- client/src/components/SubjectSections.vue
+++ client/src/components/SubjectSections.vue
@@ -10,13 +10,28 @@
       space-y-2
     "
   >
-    <h4 class="font-semibold text-gray-450-spotify dark:text-gray-500-spotify">
-      {{ label }}
-    </h4>
+    <div
+      v-if="!!subject.length"
+      class="
+        flex
+        space-x-1
+        items-baseline
+        text-gray-450-spotify
+        dark:text-gray-500-spotify
+        justify-between
+      "
+    >
+      <h4 class="font-medium">
+        {{ label }}
+      </h4>
+      <span class="font-normal text-xs">
+        {{ scores }}/{{ maxScores }} - {{ percent }}
+      </span>
+    </div>
     <div
       v-for="item in subject"
       :key="item"
-      class="flex flex-row justify-between"
+      class="flex flex-row justify-between space-x-1"
     >
       <span>{{ item.Name }}</span>
       <span>
@@ -39,6 +54,21 @@ export default {
     label: {
       type: String,
       required: true,
+    },
+  },
+  computed: {
+    filteredSubject() {
+      return this.subject.filter((s) => s.Score !== -1);
+    },
+    scores() {
+      return this.filteredSubject.reduce((n, { Score }) => n + Score, 0);
+    },
+    maxScores() {
+      return this.filteredSubject.reduce((n, { MaxScore }) => n + MaxScore, 0);
+    },
+    percent() {
+      const percent = Math.ceil((this.scores / this.maxScores) * 100) || 0;
+      return `${percent}%`;
     },
   },
 };
