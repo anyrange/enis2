4439a7329da7eb054f3cbce68d1178332e1131ba
Alex Sehl <widesehl@gmail.com>
2021-12-25 13:00:16 +0600

    chore: domain transfer

--- server/plugins/cookieOptions.js
+++ server/plugins/cookieOptions.js
@@ -2,6 +2,8 @@ import fp from "fastify-plugin";
 
 const isDev = process.env.NODE_ENV !== "production";
 
+const serverDomain = process.env.VITE_SERVER_URL;
+
 const plugin = fp(async function plugin(fastify) {
   const year = 60 * 60 * 24 * 365;
   fastify.decorate("cookieOptions", {
@@ -10,7 +12,9 @@ const plugin = fp(async function plugin(fastify) {
     httpOnly: true,
     maxAge: year,
     secure: true,
-    ...(!isDev && { domain: "enis2.ml" }),
+    ...(!isDev && {
+      domain: serverDomain,
+    }),
   });
 });
 export default plugin;
