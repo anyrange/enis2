9bed05766c53224133531f08bdf643d2f4f0bf4b
Alex Sehl <widesehl@gmail.com>
2022-03-12 14:45:28 +0600

    fix: password validation

--- client/src/views/Login.vue
+++ client/src/views/Login.vue
@@ -158,7 +158,7 @@ watch(
 
 const validateForm = ({ login, password }) => {
   form.login.valid = login.length === 12;
-  form.password.valid = password.length >= 6;
+  form.password.valid = password.length > 0;
 };
 
 const submit = async () => {
