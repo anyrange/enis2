8f2bfb9b6092dd883d2d46d9f0cf44f4493ab0dd
Alex Sehl <widesehl@gmail.com>
2022-05-15 13:26:17 +0600

    feat: next year

--- client/src/store/modules/years.js
+++ client/src/store/modules/years.js
@@ -34,21 +34,27 @@ export default defineStore("years", () => {
     try {
       const data = await getYears();
 
-      const actualYear = data.find((year) => year.isActual);
-
-      actual.value = shorterYearName(actualYear.Name);
-      settings.value.year = settings.value.year || actual.value;
-
-      const actualYearIndex = data.findIndex((year) => year.isActual);
-
-      years.value = data
-        .slice(actualYearIndex, actualYearIndex + 3)
-        .map(({ Id: value, Name: label, isActual }) => ({
+      const reversedYears = data.reverse(); // 2022, 2021 => 2021, 2022
+      const formattedYears = reversedYears.map(
+        ({ Id: value, Name: label, isActual }) => ({
           value,
           label: shorterYearName(label),
           isActual,
-        }))
-        .reverse();
+        })
+      );
+      const actualYearIndex = data.findIndex((year) => year.isActual);
+      const firstYearIndex = formattedYears.findIndex((year) => {
+        /*
+          (hardcoded) 
+          The first year that started to work with the new data format
+          It is impossible to see marks for previous years
+        */
+        return year.label === "2019-2020";
+      });
+      years.value = formattedYears.slice(firstYearIndex, actualYearIndex + 2);
+
+      actual.value = formattedYears.find((year) => year.isActual).label;
+      settings.value.year = settings.value.year || actual.value;
     } catch (error) {
       return Promise.reject(error);
     }
