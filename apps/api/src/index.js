76f3281650217a1001f4a7919b2e07518518e5cd
Alex <widesehl@gmail.com>
2023-01-07 15:54:32 +0600

    feat: publish api docs (#22)

--- apps/api/src/index.js
+++ apps/api/src/index.js
@@ -2,7 +2,7 @@ import { fileURLToPath } from "url"
 import { dirname, join } from "path"
 import fastify from "fastify"
 import autoload from "@fastify/autoload"
-import { PROD, PORT, SECRET } from "./config/index.js"
+import { PORT, SECRET } from "./config/index.js"
 
 const __dirname = dirname(fileURLToPath(import.meta.url))
 
@@ -13,24 +13,23 @@ app.register(import("@fastify/cors"), {
   credentials: true,
 })
 
-if (!PROD) {
-  app.register(import("@fastify/swagger"), {
-    routePrefix: "/docs",
-    swagger: {
-      info: {
-        title: "enis2",
-        description: "enis2 API documentation",
-      },
+app.register(import("@fastify/swagger"), {
+  routePrefix: "/docs",
+  swagger: {
+    info: {
+      title: "enis2",
+      description: "enis2 API documentation",
     },
-    uiConfig: {
-      deepLinking: true,
-      docExpansion: "none",
-      displayRequestDuration: true,
-    },
-    exposeRoute: true,
-  })
-  console.log(`Docs on: http://localhost:${PORT}/docs`)
-}
+  },
+  uiConfig: {
+    deepLinking: true,
+    docExpansion: "none",
+    displayRequestDuration: true,
+  },
+  exposeRoute: true,
+})
+
+console.log(`Docs on: http://localhost:${PORT}/docs`)
 
 app.register(import("@fastify/compress"))
 
