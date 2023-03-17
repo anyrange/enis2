3e8f4f8a41b359975dd83451f6d05ca3bf0ba8db
thaseyor <mikeston@mail.ru>
2023-03-17 21:12:55 +0300

    Revert "feat: all sms status check"
    This reverts commit adafb2b081007f2d17d931fde1cc636d842b0163.

--- apps/api/src/routes/health/sms/index.js
+++ apps/api/src/routes/health/sms/index.js
@@ -1,46 +1,6 @@
 import fetch from "node-fetch"
-import schools from "nis-schools"
 
 const SECOND = 1000
-
-const schoolCodes = schools.map(({ value }) => value)
-
-const smsStatus = {}
-schoolCodes.forEach((code) => (smsStatus[code] = true))
-
-const getStatus = (key) => {
-  if (key) return { alive: smsStatus[key] }
-
-  return smsStatus
-}
-
-let isValidStatus = false
-
-const fetchStatus = async (code) => {
-  const controller = new AbortController()
-  const timeoutId = setTimeout(() => controller.abort(), 15 * SECOND)
-
-  try {
-    const res = await fetch(`https://sms.${code}.nis.edu.kz/root`, {
-      redirect: "manual",
-      signal: controller.signal,
-    })
-
-    clearTimeout(timeoutId)
-
-    return res.status < 400
-  } catch {
-    return false
-  }
-}
-
-const updateStatus = async () => {
-  const newStates = await Promise.all(
-    schoolCodes.map((code) => fetchStatus(code))
-  )
-
-  newStates.forEach((status, id) => (smsStatus[schoolCodes[id]] = status))
-}
 
 export default async function (fastify) {
   fastify.get(
@@ -52,15 +12,24 @@ export default async function (fastify) {
       },
     },
     async (req, reply) => {
-      if (isValidStatus) return reply.send(getStatus(req.query.city))
+      const controller = new AbortController()
+
+      const timeoutId = setTimeout(() => controller.abort(), 15 * SECOND)
 
       try {
-        await updateStatus()
-        isValidStatus = true
+        const res = await fetch(
+          `https://sms.${req.query.city}.nis.edu.kz/root`,
+          {
+            redirect: "manual",
+            signal: controller.signal,
+          }
+        )
 
-        setTimeout(() => (isValidStatus = false), 60 * SECOND)
-      } finally {
-        reply.send(getStatus(req.query.city))
+        clearTimeout(timeoutId)
+
+        return reply.send({ alive: res.status < 400 })
+      } catch {
+        return reply.send({ alive: false })
       }
     }
   )
